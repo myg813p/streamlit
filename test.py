998f1220da8c6a6c4ac240dd905ee68bf930bfd9
myg813p <119914592+myg813p@users.noreply.github.com>
2022-12-20 10:50:28 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as ps
+
+view = [100,150,30,40]
+st.write('# text write 1')
+st.write('## text write 2')
+st.write('### text write 3')
+view
+st.bar_chart(view)
